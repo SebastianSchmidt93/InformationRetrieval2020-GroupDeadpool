5e3aeb453cc01d93f0b32643d2586715f1fadaa6
SebastianSchmidt93 <schmidt.ostrau@hotmail.de>
2020-12-18 14:45:06 +0100

    docker

---
+++ my-software.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#input directory
+# $2  
+#output directory
+# $3
+
+cd Docker
+docker build --tag ir_deadpool:recent .
+docker run -ti --detach --name IRgroupDeadpool -p 80:88 -v ${PWD}/Files_To_Index/:/input-dir ir_deadpool:recent
+#docker exec -ti IRgroupDeadpool "/bin/bash"
+
+
+#erstelle den Ordner für input und output
+#Packe beide Ordner in den Docker Container
+#mounte die Dateien für den Input Ordner in den Docker-Container
+#gehe in den Container, führe das Programm dort drin aus
+
+#-i input directory -o output directory
